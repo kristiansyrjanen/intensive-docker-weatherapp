a5a210aefcb5d3fcb9097bb525f2715928c84bf8
Kristian Syrjänen <35492474+kristiansyrjanen@users.noreply.github.com>
2020-01-18 18:35:45 +0200

    Add files via upload

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 const baseURL = process.env.ENDPOINT;
 const getWeatherFromApi = async () => {
   try {
-    const response = await fetch(`${baseURL}/weather`);
+    const response = await fetch(`http://localhost:8888/api/weather`);
     return response.json();
   } catch (error) {
     console.error(error);
@@ -16,21 +16,25 @@ class Weather extends React.Component {
     this.state = {
       icon: "",
       temperature: "",
+      location_city: "",
+      location_country: "",
     };
   }
   async componentWillMount() {
     const response = await getWeatherFromApi();
     console.log(response);
-    this.setState({icon: response.weather[0].icon.slice(0, -1),temperature: response.main.temp});
+    this.setState({icon: response.weather[0].icon.slice(0, -1),temperature: response.main.temp,location_city: response.name,location_country: response.sys.country});
   }
   render() {
     const { icon } = this.state;
     const { temperature } = this.state;
+    const { location_city } = this.state;
+    const { location_country } = this.state;
     return (
       <div className="icon">
-        <p>Hello World</p>
         { icon && <img src={`/img/${icon}.svg`} /> }
         <p>Temperature: {temperature}</p>
+        <p>Location: {location_city}, {location_country}</p>
       </div>
     );
   }
