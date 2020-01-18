892628eaf70b1afb17d659b82c5154a9328c6461
Kristian Syrjänen <35492474+kristiansyrjanen@users.noreply.github.com>
2020-01-18 21:42:56 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '5b47df4a7c1e7f2c7c2261d4885be74b';
+const appId = process.env.APPID || 'api-key';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
